426b89cd0385626276fffc0138723f4285a46b6c
ml-pl-ffn <69019962+ml-pl-ffn@users.noreply.github.com>
2022-01-26 12:41:19 -0800

    Modified html script to output recommendations

--- server.py
+++ server.py
@@ -11,7 +11,7 @@ model = joblib.load("models/pipe_nmf_model_checkpoint.joblib")
 nmf_model = model['pipeline']
 
 # route post requests
-@app.route('/')
+@app.route("/")
 def my_form():
     return render_template('Fintech_Credit.html')
 
@@ -21,9 +21,9 @@ def predict():
     input=[text]
     preds = nmf_model.transform(input)
     topic = preds.argmax(axis = 1)
-    predictions = [nmf_model_pipeline_param['mytopic_dict'][key] for key in topic]
+    predictions = [model['mytopic_dict'][key] for key in topic]
     output = ''.join(predictions).capitalize()
     return render_template('Fintech_Credit.html', final = output)
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', debug=True)
+  app.run(debug=True)
