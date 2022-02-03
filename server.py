9b91a16fca83d6d35893d39abb92ce3783c28f9a
ml-pl-ffn <maninagaraj@gmail.com>
2022-02-03 12:25:34 -0800

    changed server.py and html file to render card. removed additional tags to get rid of the undefined error

--- server.py
+++ server.py
@@ -11,11 +11,14 @@ app = Flask(__name__, template_folder='templates')
 model = joblib.load("models/pipe_nmf_model_checkpoint.joblib")
 nmf_model = model['pipeline']
 #Load the lookup table
-
 data = pd.read_csv('data/Credit Card agreements and rates data.csv')
 d= data.groupby(['category'],as_index=False).apply(lambda x: x.nsmallest(3, 'Min_APR'))
 d.reset_index(inplace=True)
-# print(d.head())
+# print(d.loc[d['category'] == 'Credit repair', 'Card_name'].values)
+# print(d.loc[d['category'] == 'Credit repair', 'Card_name'].values[0])
+#
+# print(reco_card[0])
+# print(reco_url[0])
 
 # route post requests
 @app.route("/")
@@ -30,10 +33,9 @@ def predict():
     topic = preds.argmax(axis = 1)
     predictions = [model['mytopic_dict'][key] for key in topic]
     output = ''.join(predictions).capitalize()
-    card = d.loc[d['category'] == 'Credit repair', 'Card_name'].values
-    url = d.loc[d['category'] == 'Credit repair', 'url'].values
-    return render_template('Fintech_Credit.html', final = output, card = card, card_url = url)
-
+    reco_card = d.loc[d['category'] == 'Credit repair', 'Card_name'].values
+    reco_url = d.loc[d['category'] == 'Credit repair', 'url'].values
+    return render_template('Fintech_Credit.html', final = output, final1 = reco_card, final2 = reco_url)
 
 if __name__ == '__main__':
   app.run(debug=True)
