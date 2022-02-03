b3200d99de9341cbb9c5e9a699f1c5eb5313500c
ml-pl-ffn <maninagaraj@gmail.com>
2022-02-03 11:26:57 -0800

    changed server.py and html to render card info.

--- server.py
+++ server.py
@@ -11,10 +11,11 @@ app = Flask(__name__, template_folder='templates')
 model = joblib.load("models/pipe_nmf_model_checkpoint.joblib")
 nmf_model = model['pipeline']
 #Load the lookup table
-filename = "data/Credit Card agreements and rates data.csv"
-data = pd.read_csv(filename)
+
+data = pd.read_csv('data/Credit Card agreements and rates data.csv')
 d= data.groupby(['category'],as_index=False).apply(lambda x: x.nsmallest(3, 'Min_APR'))
 d.reset_index(inplace=True)
+# print(d.head())
 
 # route post requests
 @app.route("/")
@@ -31,8 +32,8 @@ def predict():
     output = ''.join(predictions).capitalize()
     card = d.loc[d['category'] == 'Credit repair', 'Card_name'].values
     url = d.loc[d['category'] == 'Credit repair', 'url'].values
-    return render_template('Fintech_Credit.html', final = output)
-    # , card = card, card_url = url
+    return render_template('Fintech_Credit.html', final = output, card = card, card_url = url)
+
 
 if __name__ == '__main__':
   app.run(debug=True)
