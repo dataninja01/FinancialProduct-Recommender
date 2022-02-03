c7ed3201ea8abfe95a358dd980c8546a7cb7bbbc
ml-pl-ffn <maninagaraj@gmail.com>
2022-02-02 18:35:36 -0800

    Added lookup file and json prediction to html file

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import joblib
 import sys
 from flask import Flask, request, render_template
@@ -9,21 +10,28 @@ app = Flask(__name__, template_folder='templates')
 # load model w metadata
 model = joblib.load("models/pipe_nmf_model_checkpoint.joblib")
 nmf_model = model['pipeline']
+#Load the lookup table
+filename = "data/Credit Card agreements and rates data.csv"
+data = pd.read_csv(filename)
+d= data.groupby(['category'],as_index=False).apply(lambda x: x.nsmallest(3, 'Min_APR'))
+d.reset_index(inplace=True)
 
 # route post requests
 @app.route("/")
 def my_form():
     return render_template('Fintech_Credit.html')
 
-@app.route("/", methods = ["POST"])
+@app.route("/", methods = ["GET", "POST"])
 def predict():
-    text = request.form['name']
+    text = request.form['textbox']
     input=[text]
     preds = nmf_model.transform(input)
     topic = preds.argmax(axis = 1)
     predictions = [model['mytopic_dict'][key] for key in topic]
     output = ''.join(predictions).capitalize()
-    return render_template('Fintech_Credit.html', final = output)
+    card = d.loc[d['category'] == 'Credit repair', 'Card_name'].values
+    url = d.loc[d['category'] == 'Credit repair', 'url'].values
+    return render_template('Fintech_Credit.html', final = output, card = card, card_url = url)
 
 if __name__ == '__main__':
   app.run(debug=True)
