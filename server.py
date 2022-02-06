12ff0b249d0d70fa13218b24d876587cc26a6c07
DaisyXinyiHe <69556141+DaisyXinyiHe@users.noreply.github.com>
2022-02-06 01:41:19 -0500

    Update server.py

--- server.py
+++ server.py
@@ -3,93 +3,93 @@ import pandas as pd
 import joblib
 import sys
 from flask import Flask, request, render_template, session, jsonify
-import urllib.request
-from pusher import Pusher
-from datetime import datetime
-import httpagentparser
-import json
-import os
-import hashlib
-from dbsetup import create_connection, create_session, update_or_create_page, select_all_sessions, select_all_user_visits, select_all_pages
+# import urllib.request
+# from pusher import Pusher
+# from datetime import datetime
+# import httpagentparser
+# import json
+# import os
+# import hashlib
+# from dbsetup import create_connection, create_session, update_or_create_page, select_all_sessions, select_all_user_visits, select_all_pages
 
 
 # instantiate Flask app
 app = Flask(__name__, template_folder='templates')
 
-app.secret_key = os.urandom(24)
+# app.secret_key = os.urandom(24)
 
-## Add pusher app credential
-pusher = Pusher(app_id='PUSHER_APP_ID', key='PUSHER_APP_KEY', secret='PUSHER_APP_SECRER', cluster='PUSHER_APP_CLUSTER')
+# ## Add pusher app credential
+# pusher = Pusher(app_id='PUSHER_APP_ID', key='PUSHER_APP_KEY', secret='PUSHER_APP_SECRER', cluster='PUSHER_APP_CLUSTER')
 
-## Setup database
-database = "./pythonsqlite.db"
-conn = create_connection(database)
-c = conn.cursor()
-userOS = None
-userIP = None
-userCity = None
-userBrowser = None
-userCountry = None
-userContinent = None
-sessionID = None
+# ## Setup database
+# database = "./pythonsqlite.db"
+# conn = create_connection(database)
+# c = conn.cursor()
+# userOS = None
+# userIP = None
+# userCity = None
+# userBrowser = None
+# userCountry = None
+# userContinent = None
+# sessionID = None
 
-## Setup data format
-def main():
-    global conn, c
+# ## Setup data format
+# def main():
+#     global conn, c
     
-def parseVisitor(data):
-    update_or_create_page(c,data)
-    pusher.trigger(u'pageview', u'new', {
-        u'page': data[0],
-        u'session': sessionID,
-        u'ip': userIP
-    })
-    pusher.trigger(u'numbers', u'update', {
-        u'page': data[0],
-        u'session': sessionID,
-        u'ip': userIP
-    })
+# def parseVisitor(data):
+#     update_or_create_page(c,data)
+#     pusher.trigger(u'pageview', u'new', {
+#         u'page': data[0],
+#         u'session': sessionID,
+#         u'ip': userIP
+#     })
+#     pusher.trigger(u'numbers', u'update', {
+#         u'page': data[0],
+#         u'session': sessionID,
+#         u'ip': userIP
+#     })
 
-@app.before_request
-def getAnalyticsData():
-    global userOS, userBrowser, userIP, userContinent, userCity, userCountry,sessionID 
-    userInfo = httpagentparser.detect(request.headers.get('User-Agent'))
-    userOS = userInfo['platform']['name']
-    userBrowser = userInfo['browser']['name']
-    userIP = "72.229.28.185" if request.remote_addr == '0.0.0.0' else request.remote_addr
-    api = "https://www.iplocate.io/api/lookup/" + userIP
-    try:
-        resp = urllib.request.urlopen(api)
-        result = resp.read()
-        result = json.loads(result.decode("utf-8"))                                                                                                     
-        userCountry = result["country"]
-        userContinent = result["continent"]
-        userCity = result["city"]
-    except:
-        print("Could not find: ", userIP)
-    getSession()
+# @app.before_request
+# def getAnalyticsData():
+#     global userOS, userBrowser, userIP, userContinent, userCity, userCountry,sessionID 
+#     userInfo = httpagentparser.detect(request.headers.get('User-Agent'))
+#     userOS = userInfo['platform']['name']
+#     userBrowser = userInfo['browser']['name']
+#     userIP = "72.229.28.185" if request.remote_addr == '0.0.0.0' else request.remote_addr
+#     api = "https://www.iplocate.io/api/lookup/" + userIP
+#     try:
+#         resp = urllib.request.urlopen(api)
+#         result = resp.read()
+#         result = json.loads(result.decode("utf-8"))                                                                                                     
+#         userCountry = result["country"]
+#         userContinent = result["continent"]
+#         userCity = result["city"]
+#     except:
+#         print("Could not find: ", userIP)
+#     getSession()
     
-def getSession():
-    global sessionID
-    time = datetime.now().replace(microsecond=0)
-    if 'user' not in session:
-        lines = (str(time)+userIP).encode('utf-8')
-        session['user'] = hashlib.md5(lines).hexdigest()
-        sessionID = session['user']
-        pusher.trigger(u'session', u'new', {
-            u'ip': userIP,
-            u'continent': userContinent,
-            u'country': userCountry,
-            u'city': userCity,
-            u'os': userOS,
-            u'browser': userBrowser,
-            u'session': sessionID,
-            u'time': str(time),
-        })
-        data = [userIP, userContinent, userCountry, userCity, userOS, userBrowser, sessionID, time]
-        create_session(c,data)
-    else:
-        sessionID = session['user']
+# def getSession():
+#     global sessionID
+#     time = datetime.now().replace(microsecond=0)
+#     if 'user' not in session:
+#         lines = (str(time)+userIP).encode('utf-8')
+#         session['user'] = hashlib.md5(lines).hexdigest()
+#         sessionID = session['user']
+#         pusher.trigger(u'session', u'new', {
+#             u'ip': userIP,
+#             u'continent': userContinent,
+#             u'country': userCountry,
+#             u'city': userCity,
+#             u'os': userOS,
+#             u'browser': userBrowser,
+#             u'session': sessionID,
+#             u'time': str(time),
+#         })
+#         data = [userIP, userContinent, userCountry, userCity, userOS, userBrowser, sessionID, time]
+#         create_session(c,data)
+#     else:
+#         sessionID = session['user']
 
 
 # load model w metadata
@@ -122,31 +122,31 @@ def predict():
     reco_url = d.loc[d['category'] == output, 'url'].values
     return render_template('Fintech_Credit.html', final = output, final1 = reco_card, final2 = reco_url)
 
-@app.route('/dashboard')
-def dashboard():
-    return render_template('dashboard.html')
+# @app.route('/dashboard')
+# def dashboard():
+#     return render_template('dashboard.html')
 
-@app.route('/dashboard/<session_id>', methods=['GET'])
-def sessionPages(session_id):
-    result = select_all_user_visits(c,session_id)
-    return render_template("dashboard-single.html",data=result)
+# @app.route('/dashboard/<session_id>', methods=['GET'])
+# def sessionPages(session_id):
+#     result = select_all_user_visits(c,session_id)
+#     return render_template("dashboard-single.html",data=result)
     
-@app.route('/get-all-sessions')
-def get_all_sessions():
-    data = []
-    dbRows = select_all_sessions(c)
-    for row in dbRows:
-        data.append({
-            'ip' : row['ip'],
-            'continent' : row['continent'],
-            'country' : row['country'], 
-            'city' : row['city'], 
-            'os' : row['os'], 
-            'browser' : row['browser'], 
-            'session' : row['session'],
-            'time' : row['created_at']
-        })
-    return jsonify(data)
+# @app.route('/get-all-sessions')
+# def get_all_sessions():
+#     data = []
+#     dbRows = select_all_sessions(c)
+#     for row in dbRows:
+#         data.append({
+#             'ip' : row['ip'],
+#             'continent' : row['continent'],
+#             'country' : row['country'], 
+#             'city' : row['city'], 
+#             'os' : row['os'], 
+#             'browser' : row['browser'], 
+#             'session' : row['session'],
+#             'time' : row['created_at']
+#         })
+#     return jsonify(data)
 
 if __name__ == '__main__':
   #app.run(debug=True)
